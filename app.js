2c2f22d2c22bb4cb177a59ab8f360fd0efee6f35
AndryOwl <55844738+AndryOwl@users.noreply.github.com>
2023-08-20 15:09:26 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const axios = require('axios');
 const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Sample JSON data for dishes
 let jsonData = require('./database.json');
