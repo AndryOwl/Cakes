da5e5f97c00a296703b005cd012a3074ea7e0556
AndryOwl <55844738+AndryOwl@users.noreply.github.com>
2023-08-21 15:51:01 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
+const ingredients = require('./ingredients.json');
 const app = express();
 const port = 3000;
 
@@ -45,39 +46,42 @@ app.post('/add', (req, res) => {
 
 // Route to add ingredients
 app.get('/add-ingredient', (req, res) => {
-  res.render('add-ingredient');
+  res.render('add-ingredient', { jsonData }); // Pass jsonData to the template
 });
+
 
 app.post('/add-ingredient', (req, res) => {
   const newIngredient = {
-    id: jsonData.length + 1, // You can adjust this based on your needs
+    id: ingredients.length + 1,
     name: req.body.name,
     price: parseFloat(req.body.price),
     amount: req.body.amount,
-    // Add more ingredient fields as needed
+    dishId: findDishIdByName(req.body.dishName), // Call a function to find dishId
   };
 
+  // Add the new ingredient to your ingredients array
+  ingredients.push(newIngredient);
 
-  fs.readFile('./ingredients.json', 'utf8', (err, data) => {
+  // Write the updated ingredients array back to ingredients.json
+  fs.writeFile('./ingredients.json', JSON.stringify(ingredients), err => {
     if (err) {
-      console.error('Error reading ingredients.json:', err);
-      return;
+      console.error('Error writing to ingredients.json:', err);
+      // Handle the error appropriately
+    } else {
+      console.log('Ingredient added and data written to ingredients.json');
     }
-
-    const ingredients = JSON.parse(data);
-    ingredients.push(newIngredient);
-
-    fs.writeFile('./ingredients.json', JSON.stringify(ingredients), err => {
-      if (err) {
-        console.error('Error writing to ingredients.json:', err);
-      } else {
-        console.log('Data written to ingredients.json');
-      }
-    });
   });
 
   res.redirect('/');
 });
+
+// Function to find dishId by dishName in dishes.json
+function findDishIdByName(dishName) {
+  const dish = jsonData.find(dish => dish.name === dishName);
+  return dish ? dish.id : null;
+}
+
+
 
 
 app.get('/ingredients/:dishId', (req, res) => {
